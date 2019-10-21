f97a01cc761f242ccd68cbcfa12756056e222b2e
Teguh Agung Prabowo <40784177+azuraone@users.noreply.github.com>
2019-10-21 19:59:00 +0700

    perbaikan pada input validation

--- app/views/superadmin/index.php
+++ app/views/superadmin/index.php
@@ -790,28 +790,6 @@
             </div>
             <!-- /Modal Tambah User -->
 
-
-            <script>
-                // Example starter JavaScript for disabling form submissions if there are invalid fields
-                (function() {
-                    'use strict';
-                    window.addEventListener('load', function() {
-                        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-                        var forms = document.getElementsByClassName('needs-validation');
-                        // Loop over them and prevent submission
-                        var validation = Array.prototype.filter.call(forms, function(form) {
-                            form.addEventListener('submit', function(event) {
-                                if (form.checkValidity() === false) {
-                                    event.preventDefault();
-                                    event.stopPropagation();
-                                }
-                                form.classList.add('was-validated');
-                            }, false);
-                        });
-                    }, false);
-                })();
-            </script>
-
             <!-- ======= GRAPHIC CHART ======== -->
             <!-- <div class="row">
                 <div class="col-md-12 col-lg-6">
